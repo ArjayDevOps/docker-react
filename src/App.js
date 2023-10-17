179e659b7561ccd6e11ed0b33c21963e5de73a76
arjaydevops <raul.jhon.c.talaro@gmail.com>
2023-10-17 09:30:19 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I am asdas become a Dev Ops!
         </p>
         <a
           className="App-link"
